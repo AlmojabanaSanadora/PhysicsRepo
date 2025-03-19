34a4d94c2d89ecee6e4f52a7717ceecf3edf0a36
Almojabana <vxshy@hotmail.com>
2025-03-19 00:00:32 -0500

    UI updateee

--- NightPJ/Assets/Scripts/PlayerHealth.cs
+++ NightPJ/Assets/Scripts/PlayerHealth.cs
@@ -1,13 +1,32 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public float health = 100f;
+    public static PlayerHealth instance;
+    public Slider healthBar;
+    public float maxHealth = 100f;
+    public float currentHealth;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        UpdateHealthBar();
+    }
     public void TakeDamage(float damage)
     {
-        health -= damage;
-        if (health <= 0) Invoke(nameof(ThanosPlayer), 0.1f);
+        currentHealth -= damage;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        UpdateHealthBar();
 
+        if (currentHealth <= 0)
+        {
+            Invoke(nameof(ThanosPlayer), 0.1f);
+        }
+    }
+    private void UpdateHealthBar()
+    {
+        healthBar.value = currentHealth / maxHealth;
     }
 
     private void ThanosPlayer()
